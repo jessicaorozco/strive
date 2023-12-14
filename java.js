3cc25e95fa6a721d4f45233b1133376d74fa9158
Jessica Orozco <jessicarozco@gmail.com>
2023-12-14 13:00:47 +0100

    update

---
+++ java.js
@@ -0,0 +1,8 @@
+const images = [
+    { src: "./imagenes/IMAGE_CURSO1.jpg", alt: "Image 1" },
+    { src: "./imagenes/IMAGE_CURSO2.jpg", alt: "Image 2" },
+    { src: "./imagenes/IMAGE_CURSO3.jpg", alt: "Image 3" },
+    { src: "./imagenes/IMAGE_CURSO4.jpg", alt: "Image 4" }
+  ];
+
+  
